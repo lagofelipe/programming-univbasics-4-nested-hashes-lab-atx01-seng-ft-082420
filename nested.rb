97715f718a361f2c96679602f9e084f0693ad0e3
Felipe Dolago <lagofelipe@gmail.com>
2020-07-28 18:37:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -70,14 +70,24 @@ def adding_matz
 #   :languages => ["LISP", "C"]
 # }
 # return the entire updated hash
-
-
-	programmer_hash =
- 		{  :yukihiro_matsumoto => {
-      :known_for => "Ruby",
-      :languages => ["LISP","C"]
+programmer_hash =
+  {
+    :grace_hopper => {
+      :known_for => "COBOL",
+      :languages => ["COBOL", "FORTRAN"]
+    },
+    :alan_kay => {
+      :known_for => "Object Orientation",
+      :languages => ["Smalltalk", "LISP"]
+    },
+    :dennis_ritchie => {
+      :known_for => "Unix",
+      :languages => ["C"]
     }
-       :grace_hopper => {
+  }
+  programmer_hash =
+    {
+      :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
       },
@@ -91,10 +101,8 @@ def adding_matz
       }
     }
 
-	programmer_hash = :yukihiro_matsumoto => {
-		:known_for => "Ruby",
-		:languages => ["LISP","C"]
-	}
+
+	
 
 end
 
