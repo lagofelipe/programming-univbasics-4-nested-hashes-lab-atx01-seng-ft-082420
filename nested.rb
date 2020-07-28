cfe6f8cb309030fa3d3d381cdc91ece98b8cbc2a
Felipe Dolago <lagofelipe@gmail.com>
2020-07-28 19:08:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -141,6 +141,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-programmer_hash[:dennis_ritchie][:languages] => ["C"].push("Assembly")
+programmer_hash[:dennis_ritchie][:languages]["C"].push("Assembly")
 programmer_hash
 end
